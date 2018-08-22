8220edfb466ab80b42c392d28819846b42768ad7
Alexander Stone <aloecend@gmail.com>
2018-08-21 17:43:13 -0700

    pass all the tests now.

--- test/test_server.py
+++ test/test_server.py
@@ -1,3 +1,4 @@
+import pytest
 import requests as req
 
 
@@ -8,5 +9,46 @@ def test_server_get_home_route_status_200():
 
 def test_server_get_home_route_response_content():
     response = req.get('http://127.0.0.1:5000')
-    assert '<html><body><h1>Hello world</h1></body></html>' == str(response.text)
-    assert 'Hello world' in str(response.text)
+    assert 'cowsay' in str(response.text)
+
+
+def test_server_get_bad_requests():
+    response = req.get('http://127.0.0.1:5000/dad')
+    assert response.status_code == 404
+
+
+def test_server_get_cow_route_good_status():
+    response = req.get('http://127.0.0.1:5000/cow?msg=HelloWorld')
+    assert response.status_code == 200
+    assert 'HelloWorld' in str(response.text)
+
+
+def test_server_get_cow_route_bad_status_with_data():
+    response = req.get('http://127.0.0.1:5000/cow?blargle=yargle')
+    assert response.status_code == 400
+
+
+def test_server_get_cow_route_bad_status_with_no_data():
+    response = req.get('http://127.0.0.1:5000/cow')
+    assert response.status_code == 400
+
+
+def test_server_post_cow_good_status():
+    response = req.post('http://127.0.0.1:5000/cow?msg=HelloWorld')
+    assert response.status_code == 200
+    assert 'HelloWorld' in response.text
+
+
+def test_server_post_cow_bad_path():
+    response = req.post('http://127.0.0.1:5000/cow')
+    assert response.status_code == 400
+
+
+def test_server_post_cow_bad_request():
+    response = req.post('http://127.0.0.1:5000/cow?type=HelloWorld&Glargle=Yargle')
+    assert response.status_code == 400
+
+
+def test_server_route_method_dne():
+    response = req.put('http://127.0.0.1:5000/cow')
+    assert response.status_code == 501
